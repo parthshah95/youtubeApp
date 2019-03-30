061c2cd8dacd24a36a4c48b99794de1eac1a497e
parthshah95 <parthshahgre@gmail.com>
2019-03-29 23:31:19 -0400

    Add files via upload

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
 
+const KEY = 'AIzaSyDZI2wRM0ZrE2U7odcvbd6ud6LpCb3UvwU';
+
+export default axios.create ({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params:{
+    part:'snippet',
+    maxResults:5,
+    key:KEY
+  }
+});
